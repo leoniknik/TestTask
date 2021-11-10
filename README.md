c48d9d35e75a814bb982b8bc9d7ac9c4c9a6cf0b
Kirill Volodin <volodin_kia@open.ru>
2021-11-10 15:20:19 +0300

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TestTask
+
+Setup Project
+
+# Uber DI Framework need generator
+1) brew install needle
+
+# Setup cocoapods
+2) pod install
--- TestTask/TestTask/Service/ApiScheme.swift
+++ TestTask/TestTask/Service/ApiScheme.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct ApiScheme {
     let baseAPIURL = "https://api.flickr.com/services/rest"
-    let apiKey = "7fd0f83255e25c066966996ad29d76bd"
+    let apiKey = "7fd0f83255e25c066966996ad29d76bd" // we can make it more secure by some hacks
 
     let photosSearchMethod = "flickr.photos.search"
 }
